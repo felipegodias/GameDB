835b0e5416edee8373a95f61a59f03ffa6c8de44
felipegodias <felipe.g.o.dias@outlook.com>
2022-10-08 02:26:50 +0100

    Created assertion abstraction;

---
+++ GameDB/Src/Debug/Assert.cpp
@@ -0,0 +1,13 @@
+#include "GameDB/Debug/Assert.hpp"
+
+#include <iostream>
+
+namespace Pluto::GameDB
+{
+    void Assert::OnAssertionFailed(const std::string_view expr, const std::string_view file, const int line,
+                                   const std::string_view function, const std::string_view msg)
+    {
+        std::cout << "Assertion hit! (" << expr << ") at function " << function << "\n" << msg << "\n" <<
+            file << ":" << line << std::endl;
+    }
+}
