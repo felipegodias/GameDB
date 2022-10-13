50adb4143303ea724d422e7916a0638b3916f513
felipegodias <felipe.g.o.dias@outlook.com>
2022-10-13 23:48:50 +0100

    Create Invoke-WindowsBuildAll.ps1

---
+++ Invoke-WindowsBuildAll.ps1
@@ -0,0 +1,11 @@
+cmake --preset windows-msvc-debug
+cmake --build --preset windows-msvc-debug
+
+cmake --preset windows-msvc-release
+cmake --build --preset windows-msvc-release
+
+cmake --preset windows-clang-debug
+cmake --build --preset windows-clang-debug
+
+cmake --preset windows-clang-release
+cmake --build --preset windows-clang-release
