7aebca9317e8121466967eb504af67c9a1fb2bef
felipegodias <felipe.g.o.dias@outlook.com>
2022-10-13 23:48:53 +0100

    Create Invoke-WSLBuildAll.ps1

---
+++ Invoke-WSLBuildAll.ps1
@@ -0,0 +1,11 @@
+wsl cmake --preset linux-gcc-debug
+wsl cmake --build --preset linux-gcc-debug
+
+wsl cmake --preset linux-gcc-release
+wsl cmake --build --preset linux-gcc-release
+
+wsl cmake --preset linux-clang-debug
+wsl cmake --build --preset linux-clang-debug
+
+wsl cmake --preset linux-clang-release
+wsl cmake --build --preset linux-clang-release
