56bd0f9b96ae87765e85152bb1652d7c7c2a253b
felipegodias <felipe.g.o.dias@outlook.com>
2022-11-07 20:40:00 +0000

    Update Assert.hpp

--- GameDB/Include/GameDB/Debug/Assert.hpp
+++ GameDB/Include/GameDB/Debug/Assert.hpp
@@ -1,6 +1,7 @@
 #ifndef PLUTO_GAME_DB_ASSERT_H
 #define PLUTO_GAME_DB_ASSERT_H
 
+#include <cstdlib>
 #include <string_view>
 
 namespace Pluto::GameDB
@@ -16,7 +17,7 @@ namespace Pluto::GameDB
 #ifdef NDEBUG
 #define GAME_DB_ASSERT(expr, msg) void(0)
 #else // NDEBUG
-#define GAME_DB_ASSERT(expr, msg) if (static_cast<bool>((expr))) { Pluto::GameDB::Assert::OnAssertionFailed(#expr, __FILE__, __LINE__, static_cast<const char*>(__FUNCTION__), (msg)); abort(); } void(0)
+#define GAME_DB_ASSERT(expr, msg) if (static_cast<bool>((expr))) { Pluto::GameDB::Assert::OnAssertionFailed(#expr, __FILE__, __LINE__, static_cast<const char*>(__FUNCTION__), (msg)); std::abort(); } void(0)
 #endif // NDEBUG
 
 #endif // ! PLUTO_GAME_DB_ASSERT_H
