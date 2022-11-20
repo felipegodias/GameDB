aac57fa0ce4174ea249b0841451efb7f6f2fafcf
felipegodias <felipe.g.o.dias@outlook.com>
2022-11-20 00:55:23 +0000

    Update Kernel.cpp

--- Src/libKernel/MacOS/Kernel.cpp
+++ Src/libKernel/MacOS/Kernel.cpp
@@ -1,10 +1,32 @@
 #include "GameDB/Kernel/Kernel.hpp"
 
+#include <mutex>
+#include <pwd.h>
+#include <unistd.h>
+
 namespace GDB
 {
     std::filesystem::path Kernel::GetAppDataPath()
     {
-        std::filesystem::path appData = std::filesystem::absolute("~/Library/GameDB");
+        static std::mutex getAppDataPathMutex;
+        std::lock_guard guard(getAppDataPathMutex);
+
+        const char* homeDir = getenv("HOME");
+
+        if (!homeDir)
+        {
+            if (const passwd* pwd = getpwuid(getuid()))
+            {
+                homeDir = pwd->pw_dir;
+            }
+            else
+            {
+                throw std::runtime_error("");
+            }
+        }
+
+        std::filesystem::path appData = homeDir;
+        appData /= "Library/GameDB";
         return appData;
     }
 }
