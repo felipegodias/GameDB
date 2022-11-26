73f04572e6c8b3d143891f1d8dfa41869dc42c1c
felipegodias <felipe.g.o.dias@outlook.com>
2022-11-26 18:04:17 +0000

    Update NewAllocator.cpp

--- Src/libMemory/NewAllocator.cpp
+++ Src/libMemory/NewAllocator.cpp
@@ -1,25 +1,36 @@
 #include "GameDB/Memory/NewAllocator.hpp"
 
+#include <any>
 #include <unordered_map>
 
 #include "GameDB/Memory/GeneralAllocator.hpp"
 #include "GameDB/Memory/StlContainerAllocator.hpp"
 
-namespace 
+namespace
 {
-    using SizesMap = std::unordered_map<uintptr_t, size_t, std::hash<uintptr_t>, std::equal_to<>, GDB::StlContainerAllocator<std::pair<const uintptr_t, size_t>, GDB::GeneralAllocator>>;
-
+    using SizesMap = std::unordered_map<uintptr_t, size_t, std::hash<uintptr_t>, std::equal_to<>,
+                                        GDB::StlContainerAllocator<
+                                            std::pair<const uintptr_t, size_t>, GDB::GeneralAllocator>>;
     SizesMap* GetSizesMap()
     {
-        static SizesMap sizesMap;
-        return &sizesMap;
+        static SizesMap* sizesMap = nullptr;
+        if (sizesMap != nullptr)
+        {
+            return sizesMap;
+        }
+
+        // Manually heap allocating so we don't need to care about when the global is gonna be destructed.
+        sizesMap = static_cast<SizesMap*>(std::malloc(sizeof(SizesMap)));
+        new (sizesMap)SizesMap();
+        return sizesMap;
     }
 }
 
 gsl::owner<void*> operator new(const size_t size) // NOLINT [readability-inconsistent-declaration-parameter-name]
 {
+    SizesMap* sizesMap = GetSizesMap();
     gsl::owner<void*> ptr = GDB::GeneralAllocator::Allocate(size);
-    GetSizesMap()->emplace(reinterpret_cast<uintptr_t>(ptr), size); // NOLINT [cppcoreguidelines-pro-type-reinterpret-cast]
+    sizesMap->emplace(reinterpret_cast<uintptr_t>(ptr), size); // NOLINT [cppcoreguidelines-pro-type-reinterpret-cast]
     return ptr;
 }
 
